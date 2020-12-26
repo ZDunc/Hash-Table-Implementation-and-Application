42d4b7b4017a5e889492e3a638fc5a7058298baf
Zach Duncan <50895014+ZDunc@users.noreply.github.com>
2020-12-26 15:30:17 -0700

    Create passwordserver.h

---
+++ passwordserver.h
@@ -0,0 +1,35 @@
+// Zach Duncan
+// Section 04
+
+#ifndef _PASSSERVER_H
+#define _PASSSERVER_H
+
+#include "hashtable.h"
+#include <string>
+
+namespace cop4530{
+
+class PassServer {
+public:
+  PassServer(std::size_t size = 101); // constructor
+  ~PassServer(); // destructor
+
+  bool load(const char* filename); // load password file into HashTable object
+  bool addUser(std::pair<std::string, std::string> & kv); // add new user and password
+  bool addUser(std::pair<std::string, std::string> && kv); // move version
+  bool removeUser(const std::string & k); // remove user and password pair
+  bool changePassword(const std::pair<std::string, std::string> & p, const std::string & newpassword);
+    // update an existing pair with a new password
+  bool find(const std::string & user) const; // check if a user exists
+  void dump() const; // print contents of HashTable object to screen
+  std::size_t size() const; // return size of HashTable
+  bool write_to_file(const char* filename) const; // save user and password to file
+private:
+  HashTable<std::string, std::string> table;
+  std::string encrypt(const std::string& str);
+
+};
+
+} // end of cop4530 namespace
+
+#endif
